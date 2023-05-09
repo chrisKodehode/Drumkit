a0df20dea5bb00fb9c0088dff9503dc0aa7cb924
haugrud123 <chrishaugrud111@gmail.com>
2023-05-09 18:09:49 +0200

    + Rest of the buttons in JS

--- drumkit.js
+++ drumkit.js
@@ -6,6 +6,10 @@ const soundList = [
     document.getElementById("clap"),
     document.getElementById("hihat"),
     document.getElementById("ride"),
+    document.getElementById("openhat"),
+    document.getElementById("snare"),
+    document.getElementById("tink"),
+    document.getElementById("tom")
 ];
 
 const playKick = () => {
@@ -19,7 +23,7 @@ const playKick = () => {
 
 const playClap = () => {
     for (let i = 0 ; i < soundList.length ; i++) {
-        if (soundList[i].id === "kick") {
+        if (soundList[i].id === "clap") {
             let clapAudio = new Audio('sounds/clap.wav');
             clapAudio.play();
         }
@@ -28,7 +32,7 @@ const playClap = () => {
 
 const playHihat = () => {
     for (let i = 0 ; i < soundList.length ; i++) {
-        if (soundList[i].id === "kick") {
+        if (soundList[i].id === "hihat") {
             let hihatAudio = new Audio('sounds/hihat.wav');
             hihatAudio.play();
         }
@@ -37,9 +41,45 @@ const playHihat = () => {
 
 const playRide = () => {
     for (let i = 0 ; i < soundList.length ; i++) {
-        if (soundList[i].id === "kick") {
+        if (soundList[i].id === "ride") {
             let rideAudio = new Audio('sounds/ride.wav');
             rideAudio.play();
+        }
+    };
+};
+
+const playOpenhat = () => {
+    for (let i = 0 ; i < soundList.length ; i++) {
+        if (soundList[i].id === "openhat") {
+            let openhatAudio = new Audio('sounds/openhat.wav');
+            openhatAudio.play();
+        }
+    };
+};
+
+const playSnare = () => {
+    for (let i = 0 ; i < soundList.length ; i++) {
+        if (soundList[i].id === "snare") {
+            let snareAudio = new Audio('sounds/snare.wav');
+            snareAudio.play();
+        }
+    };
+};
+
+const playTink = () => {
+    for (let i = 0 ; i < soundList.length ; i++) {
+        if (soundList[i].id === "tink") {
+            let tinkAudio = new Audio('sounds/tink.wav');
+            tinkAudio.play();
+        }
+    };
+};
+
+const playTom = () => {
+    for (let i = 0 ; i < soundList.length ; i++) {
+        if (soundList[i].id === "tom") {
+            let tomAudio = new Audio('sounds/tom.wav');
+            tomAudio.play();
         }
     };
 };
