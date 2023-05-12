dc1f4677fa66cf8197b6b66baaca16ee67ffb4d4
chrisKodehode <chrisa.kodehode@gmail.com>
2023-05-12 14:36:54 +0200

    + code order

--- drumkit.js
+++ drumkit.js
@@ -12,16 +12,65 @@ const tink = new Audio("sounds/tink.wav");
 const tom = new Audio("sounds/tom.wav");
 
 // Declare instrument functions which play the corresponding instrument
-function playDrums() {
+function playKick() {
   kick.play();
+}
+
+function playClap() {
   clap.play();
+}
+
+function playHihat() {
   hihat.play();
+}
+
+function playRide() {
   ride.play();
+}
+
+function playOpenhat() {
   openhat.play();
+}
+
+function playSnare() {
   snare.play();
+}
+
+function playTink() {
   tink.play();
+}
+
+function playTom() {
   tom.play();
 }
+
+// Declare a variable and insert the volume slider to it 
+let volumeSlider = document.getElementById("volumeslider");
+
+const volumeToNormalized = () => {
+  // Using eventlistener to the instrument volumes so they get changed when using the volume slider
+volumeSlider.addEventListener("input", function() {
+  // Calculating the volume value to be in between 0 and 1 aka normalized (if volume is 50% it is 0.5 and if it is 100% it is 1 etc)
+  kick.volume = volumeSlider.value / 100;
+  clap.volume = volumeSlider.value / 100;
+  hihat.volume = volumeSlider.value / 100;
+  ride.volume = volumeSlider.value / 100;
+  openhat.volume = volumeSlider.value / 100;
+  snare.volume = volumeSlider.value / 100;
+  tink.volume = volumeSlider.value / 100;
+  tom.volume = volumeSlider.value / 100;
+  });
+};
+
+const volumeDisplay = () => {
+  volumeSlider.addEventListener("input", function() {
+    volumeToNormalized();
+    const volumeCalculation = volumeSlider.value;
+    document.getElementById("volumePercentageText").innerHTML = volumeCalculation;
+  });
+};
+
+volumeDisplay();
 
 document.addEventListener('keydown', (playKick) => {
   let drum = playKick.key;
@@ -62,31 +111,3 @@ document.addEventListener('keydown', (playKick) => {
     alert("No instrument")
   }
 });
-
-// Declare a constant variable and insert the volume slider to it 
-const volumeSlider = document.getElementById("volumeslider");
-
-const volumeToNormalized = () => {
-  // Using eventlistener to the instrument volumes so they get changed when using the volume slider
-volumeSlider.addEventListener("input", function() {
-  // Calculating the volume value to be in between 0 and 1 aka normalized (if volume is 50% it is 0.5 and if it is 100% it is 1 etc)
-  kick.volume = volumeSlider.value / 100;
-  clap.volume = volumeSlider.value / 100;
-  hihat.volume = volumeSlider.value / 100;
-  ride.volume = volumeSlider.value / 100;
-  openhat.volume = volumeSlider.value / 100;
-  snare.volume = volumeSlider.value / 100;
-  tink.volume = volumeSlider.value / 100;
-  tom.volume = volumeSlider.value / 100;
-  });
-};
-
-const volumeDisplay = () => {
-  volumeSlider.addEventListener("input", function() {
-    volumeToNormalized();
-    const volumeCalculation = volumeSlider.value;
-    document.getElementById("volumePercentageText").innerHTML = volumeCalculation;
-  });
-};
-
-volumeDisplay();
